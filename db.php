b91d3697b6771af49aa0d1f19f057ff2d22811ae
hugogrychta <78852574+hugogrychta@users.noreply.github.com>
2022-03-20 18:06:41 +0100

    Add files via upload

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+//identifier le nom de base de données
+$database = "wattregion";
+//connectez-vous dans votre BDD
+//Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
+$db_handle = mysqli_connect('localhost', 'root', '' );
+$db_found = mysqli_select_db($db_handle, $database);
+echo"reussi";
+
+?>
